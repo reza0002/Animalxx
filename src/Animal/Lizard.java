3c3d37de72e96c916de526e3eaad14fef4298614
Adam <adzi1000@stud.ek.dk>
2025-09-15 14:25:28 +0200

    .

--- src/Animal/Lizard.java
+++ src/Animal/Lizard.java
@@ -1,4 +1,20 @@
 package Animal;
 
-public class Lizard {
+public class Lizard extends Dyr{
+
+    public Lizard (String navn, String type){
+        super(navn ,type);
+    }
+    public String makeSound() {
+       return("sssss");
+    }
+
+    @Override
+    public String toString(){
+        return ("Type" + type + "\n" + "Navn" + navn + makeSound());
+
+
+    }
+
+
 }
