9f0867e906411f9e5a4e14b392ee7418ddc2c648
remo0002 <remo0002@stud.ek.dk>
2025-09-15 14:01:05 +0200

    ...

--- src/Animal/Hund.java
+++ src/Animal/Hund.java
@@ -1,4 +1,12 @@
 package Animal;
 
-public class Hund {
+public class Hund extends Dyr {
+    public Hund (String navn, String type){
+        super (navn, type);
+    }
+
+    @Override
+    public void makeSound(){
+        System.out.println(navn + "Vov, Vov, Vov!");
+    }
 }
